7a9a036c6563e0dd022ff47c670c08631f9edab9
Franco Pleños <120450591+Morningsstarr@users.noreply.github.com>
2023-05-23 04:33:24 +0800

    Resolved text centering issue for enhanced mobile app appearance.

--- app/src/main/java/com/example/purrfectmatchunpacked/AnnouncementsActivity.java
+++ app/src/main/java/com/example/purrfectmatchunpacked/AnnouncementsActivity.java
@@ -17,7 +17,7 @@ public class AnnouncementsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messages);
+        setContentView(R.layout.activity_announcements);
 
         ImageView menuButton = findViewById(R.id.menuButton);
 
