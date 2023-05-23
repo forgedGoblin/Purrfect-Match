b134f26923d16bc1b8b7e8686682448077b39dec
Franco Pleños <120450591+Morningsstarr@users.noreply.github.com>
2023-05-24 06:37:49 +0800

    Implemented the implementation for the Image Buttons for Facebook Page Redirects

--- app/src/main/java/com/example/purrfectmatchunpacked/DonateActivity.java
+++ app/src/main/java/com/example/purrfectmatchunpacked/DonateActivity.java
@@ -1,23 +1,38 @@
 package com.example.purrfectmatchunpacked;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.SubMenu;
-import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.purrfectmatchunpacked.backend.Globals;
 
 public class DonateActivity extends AppCompatActivity {
 
+    private static final String[] buttonUrls = {
+            "https://www.facebook.com/catsofcebuitpark/",
+            "https://www.facebook.com/HappyTailsAnimalWelfare",
+            "https://www.facebook.com/SavingStraysCebu",
+            "https://www.facebook.com/catsofdongilargao"
+    };
+
+    private static final int[] buttonIds = {
+            R.id.btnITPark,
+            R.id.btnHappy,
+            R.id.btnSavingStrays,
+            R.id.btnDonGil
+    };
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,39 +44,50 @@ public class DonateActivity extends AppCompatActivity {
         TextView name = findViewById(R.id.tvName);
         name.setText(Globals.currentUser.fname);
 
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PopupMenu popupMenu = new PopupMenu(DonateActivity.this, view);
-                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+        menuButton.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(DonateActivity.this, view);
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
 
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.account:
-                                startActivity(new Intent(DonateActivity.this, AccountActivity.class));
-                                return true;
-                            case R.id.wallet:
-                                startActivity(new Intent(DonateActivity.this, WalletActivity.class));
-                                return true;
-                            case R.id.settings:
-                                startActivity(new Intent(DonateActivity.this, SettingsActivity.class));
-                                return true;
-                            default:
-                                return false;
-                        }
-                    }
-                });
-
-                if (popupMenu.getMenu().getItem(0).hasSubMenu()) {
-                    SubMenu subMenu = popupMenu.getMenu().getItem(0).getSubMenu();
-                    if (subMenu != null && subMenu.getItem() != null) {
-                        subMenu.getItem().getIcon().setColorFilter(Color.parseColor("#F0FFFF"), PorterDuff.Mode.SRC_ATOP);
-                    }
+            popupMenu.setOnMenuItemClickListener(item -> {
+                switch (item.getItemId()) {
+                    case R.id.account:
+                        startActivity(new Intent(DonateActivity.this, AccountActivity.class));
+                        return true;
+                    case R.id.wallet:
+                        startActivity(new Intent(DonateActivity.this, WalletActivity.class));
+                        return true;
+                    case R.id.settings:
+                        startActivity(new Intent(DonateActivity.this, SettingsActivity.class));
+                        return true;
+                    default:
+                        return false;
                 }
-                popupMenu.show();
+            });
+
+            if (popupMenu.getMenu().getItem(0).hasSubMenu()) {
+                SubMenu subMenu = popupMenu.getMenu().getItem(0).getSubMenu();
+                if (subMenu != null && subMenu.getItem() != null) {
+                    subMenu.getItem().getIcon().setColorFilter(Color.parseColor("#F0FFFF"), PorterDuff.Mode.SRC_ATOP);
+                }
             }
+            popupMenu.show();
         });
+
+        for (int i = 0; i < buttonIds.length; i++) {
+            ImageButton button = findViewById(buttonIds[i]);
+            final String url = buttonUrls[i];
+            button.setOnClickListener(v -> openBrowser(url));
+        }
     }
+
+    private void openBrowser(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Verify if there is a web browser app available to handle the intent
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
